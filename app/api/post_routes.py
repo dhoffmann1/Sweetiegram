b0a898b1123374cd795b3f74dd169f099685fbac
tiffany yang <tiffanyang2015@gmail.com>
2022-09-18 01:31:19 -0700

    join query is functioning in get posts route

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -14,13 +14,13 @@ def index():
     # posts = Post.query.join(User).filter(Post.owner_id.in_([2,3,4,5])).all()
     # TODO: querying the join table: follows
     UserAlias = aliased(User)
-    users_following = [
+    following_ids = [
         user.id for user in
         User.query.join(UserAlias.followers)
         .with_entities(UserAlias).filter(User.id == current_user.id).all()
     ]
-    print('num following')
-    return {"following": str(users_following)}
-    posts = Post.query.join(User).filter(Post.owner_id.in_()).all()
+    # print('num following')
+    # return {"following": str(users_following)}
+    posts = Post.query.join(User).filter(Post.owner_id.in_(following_ids)).all()
     print("length of posts:", len(posts))
     return {"posts": [post.to_dict() for post in posts]}
