d76a7e6ab682065b83b2e227327368d9f41d62e4
JB Kam <jb3.kam@gmail.com>
2022-09-18 19:06:46 -0700

    first comments route done

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,15 +1,13 @@
 from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
-from app.models import User, db, Post, follows
+from app.models import User, db, Post, follows, Comment
 from app.forms import LoginForm, SignUpForm, PostForm
 # added here
 from sqlalchemy.orm import aliased
 
-comment_routes = Blueprint('comments', __name__, url_prefix="/api/posts")
+comment_routes = Blueprint('comments', __name__)
 
-@comment_routes.route("/<int:id>/comments")
+@comment_routes.route("/<int:id>/comments", methods=["GET"])
 def get_comments(id):
     comments = Comment.query.get(id)
-    print("comments", comments)
-    # comments = Comment.query.all()
     return comments.to_dict()
