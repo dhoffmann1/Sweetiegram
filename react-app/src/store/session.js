ef5ef92106b40dba2bd854755e89a93833380f99
tiffany yang <tiffanyang2015@gmail.com>
2022-09-20 08:56:00 -0700

    added modal css and post details modal

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -71,7 +71,7 @@ export const logout = () => async (dispatch) => {
 };
 
 
-export const signUp = (username, email, password) => async (dispatch) => {
+export const signUp = (username, email, password, bio, profilePicUrl, firstname, lastname) => async (dispatch) => {
   const response = await fetch('/api/auth/signup', {
     method: 'POST',
     headers: {
@@ -81,6 +81,11 @@ export const signUp = (username, email, password) => async (dispatch) => {
       username,
       email,
       password,
+      bio,
+      "profile_pic_url": profilePicUrl,
+      "first_name": firstname,
+      "last_name": lastname
+
     }),
   });
 
