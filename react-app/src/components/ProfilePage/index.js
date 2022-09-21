838a3410c8fae1eae7f9cfabec4520725c148c17
tiffany yang <tiffanyang2015@gmail.com>
2022-09-20 22:09:42 -0700

    added edit profile and svg images to user profile

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -38,11 +38,14 @@ const ProfilePage = () => {
                     <div className='profile-text-box'>
                         <div className='username-box profile-page-text-row'>
                             <h3 className="username-title">{user.username}</h3>
+                            <div style={{marginLeft: "8px", border:"1px solid #DBDBDB", borderRadius:"3px", display:"flex", alignItems:"center"}}>Edit profile</div>
+                            <svg style={{marginLeft: "8px"}} aria-label="Options" class="_ab6-" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" fill="none" r="8.635" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle><path d="M14.232 3.656a1.269 1.269 0 01-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 01-.796.66m-.001 16.688a1.269 1.269 0 01.796.66l.505.996h1.862l.505-.996a1.269 1.269 0 01.796-.66M3.656 9.768a1.269 1.269 0 01-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 01.66.796m16.688-.001a1.269 1.269 0 01.66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 01-.66-.796M7.678 4.522a1.269 1.269 0 01-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 01-.096 1.03m11.8 11.799a1.269 1.269 0 011.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 01.096-1.03m-14.956.001a1.269 1.269 0 01.096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 011.03.096m11.799-11.8a1.269 1.269 0 01-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 01-1.03-.096" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+
                         </div>
                         <div className='posts-follows-following-box profile-page-text-row'>
-                            <div>{user.numPosts} Posts</div>
-                            <div>{user.numFollowers} Followers</div>
-                            <div>{user.numFollowing} Following</div>
+                            <div><b>{user.numPosts}</b> Posts</div>
+                            <div style={{marginLeft:"30px"}}><b>{user.numFollowers}</b> Followers</div>
+                            <div style={{marginLeft:"30px"}}><b>{user.numFollowing}</b> Following</div>
                         </div>
                         <div className='firstname-lastname-box profile-page-text-row'>
                             <h3>{user.firstName} {user.lastName}</h3>
@@ -53,7 +56,7 @@ const ProfilePage = () => {
                     <div className='users-following-links-box'>
                         {followings.length>0 && followings.map(user=> (
                             <div className='user-following-profile-link-container'>
-                                <img className="user-following-profile-pic" src={user.profilePicUrl}/>
+                                {user && (<img className="user-following-profile-pic" src={user.profilePicUrl}/>)}
                                 <p className="user-following-full-name">{user.firstName} {user.lastName}</p>
                             </div>
                         ))}
@@ -62,20 +65,30 @@ const ProfilePage = () => {
                 <div className="third-profile-container">
                     <div className='posts-saved-container'>
                             <div className='posts-saved-button'>
-                                <i class="fa-solid fa-star"></i>
-                                <p>POSTS</p>
+                                <svg aria-label="" class="_ab6-" color="#262626" fill="#262626" height="12" role="img" viewBox="0 0 24 24" width="12"><rect fill="none" height="18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="3"></rect><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="9.015" x2="9.015" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="14.985" x2="14.985" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="9.015" y2="9.015"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="14.985" y2="14.985"></line></svg>
+                                <p style={{marginLeft:"3px"}}>POSTS</p>
                             </div>
                             <div className='posts-saved-button'>
-                                <i class="fa-solid fa-star"></i>
-                                <p>SAVED</p>
+                                <svg aria-label="" class="_ab6-" color="#8e8e8e" fill="#8e8e8e" height="12" role="img" viewBox="0 0 24 24" width="12"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+                                <p style={{marginLeft:"3px"}}>SAVED</p>
                             </div>
                     </div>
                 </div>
                 {/* grid container below */}
                 <div className= "four-profile-container">
-                    {posts.length>0  && posts.map(post => (
-                        <img src={post.postUrl}/>
-                    )
+                    {posts.length>0? (
+                        posts.map(post => (
+                            <div className="post-image-card-container">
+                                <img className='profile-post-image-pic' src={post.postUrl}/>
+                            </div>
+                        ))
+                    ) : (
+                        <div className='no-posts-message-container'>
+                            <div className='no-posts-camera-icon-container'>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+                            <p>No posts yet</p>
+                        </div>
                     )}
                 </div>
                 {/* <>
@@ -86,9 +99,7 @@ const ProfilePage = () => {
                             }
                         </> */}
             </div>
-            {posts.length>0 && posts.map(post =>(
-                <p>{post.caption}</p>
-            ))}
+
 
         </>
     )
