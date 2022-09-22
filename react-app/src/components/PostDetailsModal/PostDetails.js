9ab6ce247ab898200ba204ff088db4978f238d32
tiffany yang <tiffanyang2015@gmail.com>
2022-09-22 16:21:57 -0700

    button rendering on post page

--- react-app/src/components/PostDetailsModal/PostDetails.js
+++ react-app/src/components/PostDetailsModal/PostDetails.js
@@ -3,11 +3,15 @@ import React from "react";
 // import { Redirect } from "react-router-dom";
 import Comments from '../Comments'
 import CommentForm from "../NewCommentForm";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import './PostDetails.css';
 
 function PostDetails({ setShowPostDetailsModal, post }) {
   // console.log('post in line7 of PostDetails file', post)
   const { caption, comments, numLikes, postUrl, user, createdAt } = post
+  const currentUser = useSelector(state => state.session.user)
+
   return (
       <div id="post-details-overall-container">
         <div id="post-details-main-grid">
@@ -25,6 +29,18 @@ function PostDetails({ setShowPostDetailsModal, post }) {
                       <img id="post-details-owner-info-profile-image" src={user.profilePicUrl} alt="post owner pic" />
                     </div>
                     <div id="post-details-owner-info-username">{user.username}</div>
+                    {currentUser.id == post.ownerId && (
+                      <div className='delete-edit-buttons-container'>
+                        <div classname='delete-button'>
+                            <button style={{backgroundColor:"white", color:"pink", border: "1px solid #DBDBDB"}}>DELETE</button>
+                        </div>
+                        <NavLink to={`/posts/${post.id}/edit`}>
+                          <div classname='edit-button'>
+                              <button style={{backgroundColor:"white", color:"pink", border: "1px solid #DBDBDB"}}>EDIT</button>
+                          </div>
+                        </NavLink>
+                      </div>
+                    )}
                   </div>
                   <div id="post-details-owner-info-three-dots">
                     <i class="fa-solid fa-ellipsis"></i>
