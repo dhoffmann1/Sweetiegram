42dca30bf66f03eba1744bf1e67cbf588a2b96b7
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-09-23 12:53:31 -0500

    following button done

--- react-app/src/components/UserProfilePage/index.js
+++ react-app/src/components/UserProfilePage/index.js
@@ -3,7 +3,7 @@ import { useParams, useHistory } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { getUserPosts } from "../../store/post"
 import { getUserDetail } from "../../store/user"
-import { getFollowings } from '../../store/following'
+import { createFollowingsThunk, getFollowings, deleteFollowingsThunk } from '../../store/following'
 import { NavLink } from "react-router-dom"
 import React from 'react'
 // import './UserProfilePage.css'
@@ -23,6 +23,9 @@ const UserProfilePage = () => {
     const currentUser = useSelector(state => state.session.user)
     const user = useSelector(state => state.users.user)
     const followings = useSelector(state => Object.values(state.followings))
+
+    const [forceRender, setForceRender] = useState(false)
+
     console.log('followings:', followings)
     // print('user id:', userId)
     // get user id from posts response from db
@@ -30,7 +33,7 @@ const UserProfilePage = () => {
 
     console.log("current user logged in:", currentUser)
     console.log('SEARCHED user obj:', user)
-    console.log('POSTS:', posts)
+    // console.log('POSTS:', posts)
 
     // const handleOpenPost = (e, post) => {
     //     setPostToShowInModal(post)
@@ -41,7 +44,7 @@ const UserProfilePage = () => {
     useEffect(() => {
         dispatch(getUserPosts(userId))
         return () => resetUserPosts()
-    }, [dispatch])
+    }, [dispatch, forceRender])
 
     // TODO: how to stop someone from sending a bad url request (eg. with ID?)
     useEffect(() => {
@@ -55,25 +58,20 @@ const UserProfilePage = () => {
                 history.push('/unknown')
                 return
             });
-    }, [dispatch])
+    }, [dispatch, forceRender])
 
     useEffect(() => {
         dispatch(getFollowings(userId))
         return () => resetFollowings()
-    }, [dispatch])
+    }, [dispatch, forceRender])
 
-    let is_following = false
-    let following_id;
-    if (user) {
-        following_id = currentUser.users_following.find(id => id == user.id)
-    }
-    // if not found => undefined
-    if (following_id) is_following = true
-
-    const clickUserFollowing = (user) => {
-        // history.push(`/users/${user}`)
-        console.log('user clicked:', user)
-    }
+    let is_following = user?.followers.includes(currentUser?.id)
+    // let following_id;
+    // if (user) {
+    //     following_id = currentUser.users_following.find(id => id == user.id)
+    // }
+    // // if not found => undefined
+    // if (following_id) is_following = true
 
     return (
         <>
@@ -86,6 +84,15 @@ const UserProfilePage = () => {
                         <div className='profile-text-box'>
                             <div className='username-box profile-page-text-row'>
                                 <h3 className="username-title">{user.username}</h3>
+                                {!is_following && currentUser.id !== user.id && <div id="profile-follow-button" onClick={() => {
+                                    dispatch(createFollowingsThunk(user.id))
+                                    setForceRender(!forceRender)
+                                }}>Follow</div>}
+                                {is_following && currentUser.id !== user.id && <div id="profile-unfollow-button" onClick={() => {
+                                    dispatch(deleteFollowingsThunk(user.id))
+                                    setForceRender(!forceRender)
+                                }}>Unfollow</div>}
+
                                 <div style={{ marginLeft: "8px", border: "1px solid #DBDBDB", borderRadius: "3px", display: "flex", alignItems: "center" }}>Edit profile</div>
                                 {/* <svg style={{ marginLeft: "8px" }} aria-label="Options" class="_ab6-" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" fill="none" r="8.635" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle><path d="M14.232 3.656a1.269 1.269 0 01-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 01-.796.66m-.001 16.688a1.269 1.269 0 01.796.66l.505.996h1.862l.505-.996a1.269 1.269 0 01.796-.66M3.656 9.768a1.269 1.269 0 01-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 01.66.796m16.688-.001a1.269 1.269 0 01.66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 01-.66-.796M7.678 4.522a1.269 1.269 0 01-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 01-.096 1.03m11.8 11.799a1.269 1.269 0 011.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 01.096-1.03m-14.956.001a1.269 1.269 0 01.096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 011.03.096m11.799-11.8a1.269 1.269 0 01-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 01-1.03-.096" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg> */}
 
--- react-app/src/store/following.js
+++ react-app/src/store/following.js
@@ -1,5 +1,10 @@
 const GET_FOLLOWINGS = 'followings/getFollowings'
 const RESET_FOLLOWINGS = 'followings/resetFollowings'
+const CREATE_FOLLOWING = "followings/createFollowings"
+const DELETE_FOLLOWING = "following/deleteFollowings"
+
+
+// AC => GET Following
 const load = (payload) => {
     return {
         type: GET_FOLLOWINGS,
@@ -7,17 +12,57 @@ const load = (payload) => {
     }
 }
 
+// AC => CREATE Following
+const createFollowing = (user) => {
+    return {
+        type: CREATE_FOLLOWING,
+        user
+    }
+}
+
+// AC => DELETE Following
+const deleteFollowings = (userId) => {
+    return {
+        type: DELETE_FOLLOWING,
+        userId
+    }
+}
+
+
+export const resetFollowings = () => {
+    return {
+        type: RESET_FOLLOWINGS
+    }
+}
+
+// AC THUNK => CREATE Following
+export const createFollowingsThunk = (userId) => async dispatch => {
+    const response = await fetch(`/api/user/${userId}/following`, {
+        method: "POST"
+    })
+    if (response.ok) {
+        let followingUser = await response.json()
+        dispatch(createFollowing(followingUser))
+    }
+}
+
+// AC THUNK => DELETE Following
+export const deleteFollowingsThunk = (userId) => async dispatch => {
+    const response = await fetch(`/api/user/${userId}/following`, {
+        method: "DELETE"
+    })
+    if (response.ok) {
+        dispatch(deleteFollowings(userId))
+    }
+}
+
+
+// AC THUNK => GET Following
 export const getFollowings = (userId) => async dispatch => {
     const response = await fetch(`/api/user/${userId}/following`)
     if (response.ok) {
         let followings = await response.json()
         dispatch(load(followings))
-    }
-}
-
-export const resetFollowings = () => {
-    return {
-        type: RESET_FOLLOWINGS
     }
 }
 
@@ -28,6 +73,16 @@ const followingReducer = (state = initialState, action) =>{
         case GET_FOLLOWINGS: {
             const newState = {}
             action.payload.following_users.forEach( user => newState[user.id] = user)
+            return newState
+        }
+        case CREATE_FOLLOWING: {
+            const newState = { ...state }
+            newState[action.user.id] = action.user
+            return newState
+        }
+        case DELETE_FOLLOWING: {
+            const newState = { ...state }
+            delete newState[action.userId]
             return newState
         }
         case RESET_FOLLOWINGS: {
